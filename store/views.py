3419d09608df0223c8bbf27af204c56ca9e20bb9
Oghenekevbe Egume <oghenekevbe.egume@gmail.com>
2023-03-24 00:27:14 +0000

    adjusted cart view to show order items in terminal

--- store/views.py
+++ store/views.py
@@ -99,6 +99,7 @@ def AddToCart(request):
         print(order)
         order_item, created = OrderItem.objects.get_or_create(order=order, product=product)
         order_item.quantity +=1
+        print(str(order_item) + ' - ' + str(order_item.quantity) + 'pcs in your cart')
         order_item.save()
 
         total_items = order.get_cart_items
